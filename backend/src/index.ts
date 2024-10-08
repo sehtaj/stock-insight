6bc273ee5035e95299b3703a6dbe6ee9cc474d58
sehtaj <batthsehtaj10@gmail.com>
2024-10-08 00:59:59 -0400

    remove console.logs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,9 +6,6 @@ import path from 'path';  // Import the path module
 dotenv.config({ path: path.resolve(__dirname, '../../.env') }); // Load environment variables from .env file
 const app = express();
 const PORT = process.env.PORT || 3001;
-// Print the specific environment variable (e.g., POLYGON_API_KEY)
-
-console.log('POLYGON_API_KEY:', process.env.POLYGON_API_KEY);
 
 app.use('/', apiRoutes);
 
