b5b6ba245b0ec6fb768f9203d10b444830fc14c2
sehtaj <batthsehtaj10@gmail.com>
2024-10-08 19:01:50 -0400

    server port dynamic functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,19 @@
 import express from 'express';
 import apiRoutes from './routes/api';
-import dotenv from 'dotenv';
 import cors from 'cors';  // Import cors
-import path from 'path';  // Import the path module
 
-dotenv.config({ path: path.resolve(__dirname, '../../.env.local') }); // Load environment variables from .env file
 const app = express();
-console.log("port", process.env.PORT);
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; // Not necessary for Vercel
 
 const allowedOrigins = process.env.CORS_ORIGIN || 'http://localhost:3000';
 
- 
 // Use CORS to allow requests from your frontend
 app.use(cors({ origin: allowedOrigins }));
 
 // Use API routes
 app.use('/', apiRoutes);
 
-// Only run app.listen() when not in production (e.g., for local development)
-console.log("backend env: ", process.env.NODE_ENV)
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    console.log("backend env: ", process.env.NODE_ENV)
+// Export the app for Vercel to use
+export default app;
 
-  });
-}
+// No need to call app.listen() in Vercel
