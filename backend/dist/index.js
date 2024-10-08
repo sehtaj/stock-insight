1d65a41a065d445324be2dac765666d6a027986d
sehtaj <batthsehtaj10@gmail.com>
2024-10-08 16:08:36 -0400

    server port dynamic functionality

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -11,11 +11,17 @@ const path_1 = __importDefault(require("path")); // Import the path module
 dotenv_1.default.config({ path: path_1.default.resolve(__dirname, '../../.env') }); // Load environment variables from .env file
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
-const allowedOrigins = process.env.CORS_ORIGIN || 'http://localhost:3000';
+// Update allowed origins to include your frontend deployed on Vercel
+const allowedOrigins = [
+    process.env.CORS_ORIGIN || 'http://localhost:3000',
+    'https://stock-insight-orpin.vercel.app', // Add your deployed frontend URL
+    'https://stock-insight-git-develop-sehtajs-projects.vercel.app' // Add any other necessary origins
+];
 // Use CORS to allow requests from your frontend
 app.use((0, cors_1.default)({ origin: allowedOrigins }));
 // Use API routes
 app.use('/', api_1.default);
+// Only run app.listen() when not in production (e.g., for local development)
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
